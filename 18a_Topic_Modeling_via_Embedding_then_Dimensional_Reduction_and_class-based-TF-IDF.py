d2087d0db47e6d01f698e6dd6b49d10b0b5e961a
alansley <aclansley@gmail.com>
2025-06-24 17:14:47 +1000

    Minor cleaning

--- 18a_Topic_Modeling_via_Embedding_then_Dimensional_Reduction_and_class-based-TF-IDF.py
+++ 18a_Topic_Modeling_via_Embedding_then_Dimensional_Reduction_and_class-based-TF-IDF.py
@@ -66,7 +66,8 @@ print(f"\n--- Created embeddings for {embeddings.shape[0]} abstracts, where each
 # handles non-linear relationships and structures better than PCA (Principal Component Analysis):
 # Further UMAP reading: https://arxiv.org/abs/1802.03426
 
-# Reduce 384-dimensional embeddings to 2 dimensions for easier visualization
+# Reduce 384-dimensional embeddings to 5 dimensions for easier visualization.
+# Note: Generally, values between 5 and 10 work well to capture high-dimensional global structures.
 reduced_dimension_count = 2
 print(f"\n--- Creating UMAP model to reduce dimensions from {embeddings.shape[1]} to {reduced_dimension_count}...")
 umap_model = UMAP(
@@ -78,11 +79,6 @@ umap_model = UMAP(
 )
 reduced_embeddings = umap_model.fit_transform(embeddings)
 
-# Confirm this shape
-print("DEBUG!!!!!!")
-print(reduced_embeddings.shape)  # Should be (n_docs, 2)
-print(len(titles))               # Should match n_docs
-
 # Now use HDBSCAN (Hierarchical Density-Based Spatial Clustering of Applications with Noise) to perform clustering.
 # As a density-based method, HDBSCAN can also detect outliers in the data, which are data points that do not belong to
 # any cluster. These outliers will not be assigned or forced to belong to any cluster. In other words, they are ignored.
@@ -90,7 +86,11 @@ print(len(titles))               # Should match n_docs
 
 # We fit the model and extract the clusters
 print(f"\n--- Creating HDBSCAN model to map embeddings into clusters...")
-hdbscan_model = HDBSCAN(min_cluster_size=50, metric="euclidean", cluster_selection_method="eom").fit(reduced_embeddings)
+hdbscan_model = HDBSCAN(
+	min_cluster_size=50,
+	metric="euclidean",
+	cluster_selection_method="eom"
+).fit(reduced_embeddings)
 clusters = hdbscan_model.labels_
 
 # How many clusters did we generate? In this instance we're expecting 152 - which is quite a lot of clusters...
@@ -98,39 +98,6 @@ clusters = hdbscan_model.labels_
 # used for outliers that don't fit cleanly into any of our other topics.
 num_clusters = len(set(clusters))
 print(f"\n--- HDBSCAN generated {num_clusters} clusters.")
-
-# ----- COMMENTED OUT
-# # Print first three documents in cluster 0 - we expect them to be about sign-language in this instance!
-# print("\n--- Our first few papers in the first cluster are all about sign-language!:")
-# cluster = 0
-# for index in np.where(clusters==cluster)[0][:3]:
-# 	print(abstracts[index][:150] + "... \n")
-#
-# # Reduce 384-dimensional embeddings to two dimensions for easier visualization
-# reduced_embeddings = UMAP(n_components=2, min_dist=0.0, metric="cosine", random_state=42).fit_transform(embeddings)
-#
-# # Create dataframe
-# clusters_df = pd.DataFrame(reduced_embeddings, columns=["x", "y"])
-# clusters_df["title"]   = titles
-# clusters_df["cluster"] = [str(c) for c in clusters]
-#
-# # Select recognised and outlier clusters
-# recognised_clusters_df = clusters_df.loc[clusters_df.cluster != "-1", :]
-# outliers_df            = clusters_df.loc[clusters_df.cluster == "-1", :]
-#
-# # Plot recognised clusters (coloured) and outliers (grey) separately.
-# # Note: Cluster colours get re-used because there's 161 of them but less than that in the colour-map - the take-away is
-# # that there are lots of clusters and that they each have their own location and shape when crunched down to 2D space.
-# plt.scatter(outliers_df.x, outliers_df.y, alpha=0.05, s=2, c="grey")
-# plt.scatter(recognised_clusters_df.x, recognised_clusters_df.y, c=recognised_clusters_df.cluster.astype(int), alpha=0.6, s=2, cmap="tab20b")
-# plt.axis("off")
-#
-# plt.show()
-#
-# # While this is visually interesting, it doesn't allow us to see what's happening inside the clusters. So rather than
-# # stopping here, we can extend this visualization by going from TEXT clustering to TOPIC modeling.
-# ----- END OF COMMENTED OUT
-
 
 # Train our model with our previously defined models
 topic_model = BERTopic(
@@ -140,15 +107,9 @@ topic_model = BERTopic(
     verbose=True
 ).fit(abstracts, embeddings)
 
-# info_df: DataFrame = topic_model.get_topic_info()
-# print("ACL11111")
-# print("Shape: " + info_df.shape)
-# print(info_df)
-# print("ACL22222")
-
 #print(topic_model.get_topic_info())
 
-print("\n--- The first topic just happens to be about speech (note: ASM is 'Automatic Speech Recognition')")
+print("\n--- The first topic just happens to be about speech (note: ASR is 'Automatic Speech Recognition')")
 print(topic_model.get_topic(0))
 
 # We can find topics by keyword - the result will look like:
@@ -158,8 +119,9 @@ print(topic_model.get_topic(0))
 # )
 #
 # This tells us that topic 7 (first value in first array) has the most similarity to the word "graphics", at 0.84470475.
-graphics_topic_query = topic_model.find_topics("graphics", top_n=5)
-print(f"\n--- The most relevant topic to graphics is:\n{graphics_topic_query}")
+topic = "graphics"
+topic_query = topic_model.find_topics(topic, top_n=5)
+print(f"\n--- The most relevant topic to {topic} is:\n{topic_query}")
 
 # Then, if we grab the graphics topics we can see the top few words along with their c-TF-IDF relevance scores, ours
 # will look like this:
@@ -175,9 +137,12 @@ print(f"\n--- The most relevant topic to graphics is:\n{graphics_topic_query}")
 #   ('captioning', 0.009682865901064713),
 #   ('caption', 0.008365740881928098)
 # ]
-graphics_topic_number = graphics_topic_query[0][0]
-graphics_topic_details = topic_model.get_topic(graphics_topic_number)
-print(f"\n--- Details for topic {graphics_topic_number} are:\n{graphics_topic_details}")
+topic_number = topic_query[0][0]
+topic_details = topic_model.get_topic(topic_number)
+print(f"\n--- Details for topic {topic_number} ({topic}) are:\n{topic_details}")
+
+# Now to visualise the data we'll reduce it from 384 dimensions to 2 dimensions (X/Y)
+#reduced_embeddings = UMAP(n_components=2, min_dist=0.0, metric="cosine", random_state=42).fit_transform(embeddings)
 
 # BERTopic uses Plotly for interactive plotting - so we'll set it to open each plot in our default browser.
 # Note: Without this `show()`-ing a figure just prints the details to the console!
@@ -186,30 +151,33 @@ pio.renderers.default = "browser"
 # Visualize topics and documents
 documents_fig = topic_model.visualize_documents(
     docs=abstracts,
-    reduced_embeddings=reduced_embeddings,
+	sample=0.025,  # We'll only visualise a random 2.5% of the documents as 44,949 is a lot! 2.5% is ~1000-ish.
+    #reduced_embeddings=reduced_embeddings,
+	embeddings=embeddings,
     hide_annotations=True
 )
 
 # Replace the hover text with the corresponding titles. Note: The scatter is in fig.data[0]
-titles_as_hover = ["Title: " + title for title in titles]
-documents_fig.data[0].hovertext = titles_as_hover
-documents_fig.data[0].hovertemplate = "%{hovertext}<extra></extra>"
+#titles_as_hover = ["Title: " + title for title in titles]
+#documents_fig.data[0].hovertext = titles_as_hover
+#documents_fig.data[0].hovertemplate = "%{hovertext}<extra></extra>"
 
 # Colour each title by topic in the legend
-topics = topic_model.topics_
-documents_fig.data[0].marker.color = topics
+#topics = topic_model.topics_
+#documents_fig.data[0].marker.color = topics
 
 # Now show the plot!
 documents_fig.show()
 
-# Visualize barchart with ranked keywords
-bar_fig = topic_model.visualize_barchart(top_n_topics=num_clusters)
+# Visualize barchart with ranked keywords - we'll only show the first dozen in this example
+bar_fig = topic_model.visualize_barchart(top_n_topics=12)
 bar_fig.show()
 
 # Visualize relationships between topics. We have 161 topics (i.e., "clusters" - but we'll just show the first 30
 heatmap_fig = topic_model.visualize_heatmap(width=1024,height=1024,n_clusters=30)
 heatmap_fig.show()
 
-# Visualize the potential hierarchical structure of topics. We'll only show the top 10 most frequent topics
-hierarchy_fig = topic_model.visualize_hierarchy(top_n_topics=11)
+# Visualize the potential hierarchical structure of topics. We'll only show the top 30 largest topics (i.e., those with
+# the most papers in the topic).
+hierarchy_fig = topic_model.visualize_hierarchy(top_n_topics=30)
 hierarchy_fig.show()
